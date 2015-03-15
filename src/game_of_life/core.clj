b1a4332b464ae32a4d5e5bb294df6a65d668a658
maggarwal <maggarwal@amplify.com>
2015-03-15 19:05:01 -0400

    some helper methods

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,6 +1,23 @@
 (ns game-of-life.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn create-matrix
+  "given dimensions m and n, this function creates m x n matrix with all nils"
+  [m n]
+  (vec (repeat m (vec (repeat n nil)))))
+
+(defn init-matrix
+  "given an empty matrix (all nils) and a set of cell inices this returns a matrix with those inices set to :on"
+  [matrix cells]
+  (reduce (fn [new-matrix cell]
+            (assoc-in new-matrix cell :on)) matrix cells))
+
+;; (defn neighbors
+;;   "given a matrix and a cell indexes, this function returns the neighbors this cell has.")
+
+(defn test-main
+  "used only for debugging purposes."
+  []
+  (let [m (create-matrix 4 4)]
+    (init-matrix m [[0 0] [1 1]])))
+
+(test-main)
